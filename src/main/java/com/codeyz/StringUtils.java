ff41f129d6078e92b01f402544143e868f9c0614
Atom <a08i80e@outlook.com>
2020-04-03 09:37:24 +0600

    StringUtils. added byteToHex method

--- src/main/java/com/codeyz/StringUtils.java
+++ src/main/java/com/codeyz/StringUtils.java
@@ -1,4 +1,20 @@
 package com.codeyz;
 
 public class StringUtils {
+
+    public static String bytesToHex(byte[] data) {
+        if (data == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder(2 * data.length);
+        for (byte b : data) {
+            int b1 = (b >> 4) & 0x0f;
+            int b2 = b & 0x0F;
+            int ch1 = (b1 >= 0x0A) ? 'A' + b1 - 0xA : '0' + b1;
+            int ch2 = (b2 >= 0x0A) ? 'A' + b2 - 0xA : '0' + b2;
+            sb.append((char) ch1);
+            sb.append((char) ch2);
+        }
+        return sb.toString();
+    }
 }
