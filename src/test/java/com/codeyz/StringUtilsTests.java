91c3553546b403b02637203152824035b2044d4b
Atom <a08i80e@outlook.com>
2020-04-05 02:06:18 +0600

    StringUtilsTests. added hexToBytes tests

--- src/test/java/com/codeyz/StringUtilsTests.java
+++ src/test/java/com/codeyz/StringUtilsTests.java
@@ -4,5 +4,24 @@ import org.junit.*;
 
 public class StringUtilsTests {
 
+    private byte[] expectedBytes = {18, -81, 119};
+
+    @Test(expected = IllegalArgumentException.class)
+    public void hexToByteErrorTest() {
+        StringUtils.hexToBytes("QWERT");
+    }
+
+    @Test
+    public void hexToBytesNullTest() {
+        byte[] result = StringUtils.hexToBytes(null);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void hexToBytesSuccsessTest() {
+        String expected = new String(expectedBytes);
+        byte[] result = StringUtils.hexToBytes("12aF77");
+        Assert.assertEquals(expected, new String(result));
+    }
 
 }
