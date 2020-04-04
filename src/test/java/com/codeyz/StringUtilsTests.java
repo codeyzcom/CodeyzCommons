f85491214202b71d713d6a5c718d78201408e9fd
Atom <a08i80e@outlook.com>
2020-04-05 02:22:38 +0600

    StringUtilsTests. Added bytesToHex tests

--- src/test/java/com/codeyz/StringUtilsTests.java
+++ src/test/java/com/codeyz/StringUtilsTests.java
@@ -4,7 +4,7 @@ import org.junit.*;
 
 public class StringUtilsTests {
 
-    private byte[] expectedBytes = {18, -81, 119};
+    private byte[] bytes = {18, -81, 119};
 
     @Test(expected = IllegalArgumentException.class)
     public void hexToByteErrorTest() {
@@ -18,10 +18,22 @@ public class StringUtilsTests {
     }
 
     @Test
-    public void hexToBytesSuccsessTest() {
-        String expected = new String(expectedBytes);
+    public void hexToBytesSuccessTest() {
+        String expected = new String(bytes);
         byte[] result = StringUtils.hexToBytes("12aF77");
         Assert.assertEquals(expected, new String(result));
+    }
+
+    @Test
+    public void bytesToHexNullTest() {
+        String result = StringUtils.bytesToHex(null);
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void bytesToHexSuccessTest() {
+        String result = StringUtils.bytesToHex(bytes);
+        Assert.assertEquals(result, "12AF77");
     }
 
 }
