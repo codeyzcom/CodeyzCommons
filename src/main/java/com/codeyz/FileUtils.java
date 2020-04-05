098eb257639174bdfe061b05cc67c2cb59d4a0cf
Atom <a08i80e@outlook.com>
2020-04-05 12:47:12 +0600

    FileUtils. Added getFileExtension method

--- src/main/java/com/codeyz/FileUtils.java
+++ src/main/java/com/codeyz/FileUtils.java
@@ -1,4 +1,22 @@
 package com.codeyz;
 
+import java.io.File;
+
 public class FileUtils {
+
+    private FileUtils() {
+    }
+
+    public static String getFileExtension(String fileName) {
+        int k = fileName.lastIndexOf('.');
+        int tmp = fileName.lastIndexOf(File.separatorChar);
+        if (tmp > k) {
+            k = tmp;
+        }
+        if (k > -1 && fileName.charAt(k) == '.') {
+            return fileName.substring(k + 1);
+        } else {
+            return null;
+        }
+    }
 }
