ddd53b0877b2de1b679fefe5d2697ae0fac6ba2a
Atom <atom@codeyz.com>
2020-04-05 13:16:54 +0600

    FileUtils. Added copy method - first implement

--- src/main/java/com/codeyz/FileUtils.java
+++ src/main/java/com/codeyz/FileUtils.java
@@ -1,8 +1,13 @@
 package com.codeyz;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileUtils {
+
+    private static final int BUFF_SIZE = 100000;
 
     private FileUtils() {
     }
@@ -18,5 +23,36 @@ public class FileUtils {
         } else {
             return null;
         }
+    }
+
+
+    /**
+     *
+     * @param src
+     * @param dest
+     * @param maxSize The maximum number of bytes. If -1, then write everything.
+     * @return Number of bytes copied.
+     * @throws IOException
+     */
+    public static int copy(InputStream src, OutputStream dest, int maxSize) throws IOException {
+        if (maxSize == 0) {
+            return 0;
+        }
+
+        final int packetSize = maxSize > 0 && BUFF_SIZE > maxSize ? maxSize : BUFF_SIZE;
+        final byte[] buffer = new byte[packetSize];
+        int res = 0;
+        do {
+            int len = src.read(buffer, 0, maxSize > 0 && packetSize + res > maxSize ? maxSize - res : packetSize);
+            if (len <= 0) {
+                break;
+            }
+            dest.write(buffer, 0, len);
+            res += len;
+            if (maxSize > 0 && res >= maxSize) {
+                break;
+            }
+        } while (true);
+        return res;
     }
 }
